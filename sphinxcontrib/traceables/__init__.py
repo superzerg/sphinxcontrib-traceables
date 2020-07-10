a4ca22d583203b05e8fda2f63b98d430b31ca3e2
Yves Renier <yves.renier@epfl.ch>
2020-07-10 12:13:46 +0200

    simplified import in __init__

--- sphinxcontrib/traceables/__init__.py
+++ sphinxcontrib/traceables/__init__.py
@@ -1,36 +1,33 @@
 
-import infrastructure
-import display
-import traceables
-import list
-import matrix
-import graph
+from .infrastructure import ProcessorManager, setup as infrastructure_setup
+from .display import TraceableDisplayProcessor, setup as display_setup
+from .traceables import RelationshipsProcessor, XrefProcessor, setup as traceables_setup
+from .list import ListProcessor, setup as list_setup
+from .matrix import MatrixProcessor, setup as matrix_setup
+from .graph import GraphProcessor, setup as graph_setup
 
 
 # ==========================================================================
 # Setup and register extension
 
 def setup(app):
-    # Perform import within this function to avoid an import circle.
-    from sphinxcontrib import traceables
-
     # Allow extension parts to set themselves up.
-    traceables.infrastructure.setup(app)
-    traceables.display.setup(app)
-    traceables.traceables.setup(app)
-    traceables.list.setup(app)
-    traceables.matrix.setup(app)
-    traceables.graph.setup(app)
+    infrastructure_setup(app)
+    display_setup(app)
+    traceables_setup(app)
+    list_setup(app)
+    matrix_setup(app)
+    graph_setup(app)
 
     # Register business logic of extension parts. This is done explicitly
     # here to ensure correct ordering during processing.
-    traceables.infrastructure.ProcessorManager.register_processor_classes([
-        traceables.traceables.RelationshipsProcessor,
-        traceables.display.TraceableDisplayProcessor,
-        traceables.traceables.XrefProcessor,
-        traceables.list.ListProcessor,
-        traceables.matrix.MatrixProcessor,
-        traceables.graph.GraphProcessor,
+    ProcessorManager.register_processor_classes([
+        RelationshipsProcessor,
+        TraceableDisplayProcessor,
+        XrefProcessor,
+        ListProcessor,
+        MatrixProcessor,
+        GraphProcessor,
     ])
 
     return {"version": "0.0"}
