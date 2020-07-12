0450510cc4ede21e3a57b3e1214b1ab2fe6093f3
Yves Renier <yves.renier@epfl.ch>
2020-07-12 11:35:49 +0200

    register sphinx marker in pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,3 +8,9 @@ pytest_plugins = 'sphinx.testing.fixtures'
 @pytest.fixture(scope='session')
 def rootdir():
     return path(__file__).parent.abspath() / 'data'
+
+
+def pytest_configure(config):
+    config.addinivalue_line(
+        "markers", "sphinx: marker for sphinx tests"
+    )
